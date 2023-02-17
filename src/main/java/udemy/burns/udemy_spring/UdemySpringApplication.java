7fca1a0466320ba62d2dd8ef25cd011e80641ac6
joel burns <jbur645@wgu.edu>
2023-02-17 11:10:23 -0700

    Initial commit

---
+++ src/main/java/udemy/burns/udemy_spring/UdemySpringApplication.java
@@ -0,0 +1,13 @@
+package udemy.burns.udemy_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/udemy/burns/udemy_spring/UdemySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.burns.udemy_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
